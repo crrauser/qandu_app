4c7030ccbf27e9d879555839e72163b96242ba0c
Casey <casey.rausr@quinnipiac.edu>
2015-10-06 18:58:36 +0000

    Add question list view

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, CreateView
 from django.core.urlresolvers import reverse_lazy
 from .models import *
+from django.views.generic import ListView
 
 
 class Home(TemplateView):
@@ -11,8 +12,12 @@ class QuestionCreateView(CreateView):
   model = Question
   template_name = "question/question_form.html"
   fields = ['title', 'description']
-  success_url = reverse_lazy('home')
+  success_url = reverse_lazy('question_list')
 
   def form_valid(self, form):
     form.instance.user = self.request.user
     return super(QuestionCreateView, self).form_valid(form)
+
+class QuestionListView(ListView):
+  model = Question
+  template_name = "question/question_list.html"
