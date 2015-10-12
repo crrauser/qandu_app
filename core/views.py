ebf756fa6d8953a6903eef97dd60948588a87f94
Casey <casey.rausr@quinnipiac.edu>
2015-10-12 15:54:45 +0000

    Add answer voting

--- core/views.py
+++ core/views.py
@@ -7,6 +7,9 @@ from django.views.generic import DetailView
 from django.views.generic import UpdateView
 from django.views.generic import DeleteView
 from django.core.exceptions import PermissionDenied
+from django.shortcuts import redirect
+from django.views.generic import FormView
+from .forms import *
 
 
 
@@ -100,3 +103,27 @@ class AnswerDeleteView(DeleteView):
     if object.user != self.request.user:
       raise PermissionDenied()
     return object
+
+class VoteFormView(FormView):
+  form_class = VoteForm
+
+  def form_valid(self, form):
+    user = self.request.user
+    question = Question.objects.get(pk=form.data["question"])
+    try:
+      answer = Answer.objects.get(pk=form.data["answer"])
+      prev_votes = Vote.objects.filter(user=user, answer=answer)
+      has_voted = (prev_votes.count()>0)
+      if not has_voted:
+        Vote.objects.create(user=user, answer=answer)
+      else:
+        prev_votes[0].delete()
+      return redirect(reverse('question_detail', args=[form.data["question"]]))
+    except:
+      prev_votes = Vote.objects.filter(user=user, question=question)
+      has_voted = (prev_votes.count()>0)
+      if not has_voted:
+        Vote.objects.create(user=user, question=question)
+      else:
+        prev_votes[0].delete()
+    return redirect('question_list')
